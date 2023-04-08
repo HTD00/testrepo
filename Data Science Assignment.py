c864af668989e09dbad9bee5fb731f6ef1b4774f
HTD00 <117132698+HTD00@users.noreply.github.com>
2023-04-07 21:38:33 -0500

    Tools For DS Final Assignment
    This is a Jupyter Notebook Assignment for Tools for Data Science Class Uploaded into Github

---
+++ Data Science Assignment.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tool and Ecosystem are summarized for the final assignment of this module
+
+# **Objectives:**
+# - Popular languages for Data Science
+# - Popular libraries for Data Science
+# - Tools used in Data Science
+
+# Some of the most popular languages used for Data Science are: 
+# 1. Python 
+# 2. R 
+# 3. Scala
+# 
+
+# Some of the most popular libraries used by data scientists are:
+# 1. Pandas
+# 2. Matplotlib
+# 3. ggplot
+
+# Data Science Tools |
+# -------------------|
+# Jupyter Notebook
+# Rstudio
+# VS Code
+
+# ### Below are some examples of evaluating arithmetic expressions in Python
+# 
+
+# In[2]:
+
+
+# This is a simple expression to multiply and add integers
+(3*4)+5
+
+
+# In[7]:
+
+
+# this will convert 200 mins into full hrs by dividing by 60
+mins= 200
+hours = int(mins/60)
+print(hours)
+
+
+# ## Author
+# Haytam 
